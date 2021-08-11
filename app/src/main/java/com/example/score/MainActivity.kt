bbaa108fc50a17eb378e7c432d23e021794a7429
fan1138612367 <fan1138612367@vip.qq.com>
2021-08-11 12:46:46 +0800

    StateFlow代替LiveData

--- app/src/main/java/com/example/score/MainActivity.kt
+++ app/src/main/java/com/example/score/MainActivity.kt
@@ -8,12 +8,12 @@ import com.example.score.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val myViewModel by viewModels<MyViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val myViewModel by viewModels<MyViewModel>()
         binding.myViewModel = myViewModel
         binding.lifecycleOwner = this
     }
--- app/src/main/java/com/example/score/MyViewModel.kt
+++ app/src/main/java/com/example/score/MyViewModel.kt
@@ -1,32 +1,32 @@
 package com.example.score
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MyViewModel : ViewModel() {
-    private val _numberA = MutableLiveData<Int>().also { it.value = 0 }
-    private val _numberB = MutableLiveData<Int>().also { it.value = 0 }
-    val numberA: LiveData<Int> = _numberA
-    val numberB: LiveData<Int> = _numberB
-    private var backA: Int = 0
-    private var backB: Int = 0
+    private val _numberA by lazy { MutableStateFlow(0) }
+    private val _numberB by lazy { MutableStateFlow(0) }
+    val numberA: StateFlow<Int> get() = _numberA
+    val numberB: StateFlow<Int> get() = _numberB
+    private var backA = 0
+    private var backB = 0
 
     fun addA(int: Int) {
-        backA = _numberA.value!!
-        backB = _numberB.value!!
-        _numberA.value = _numberA.value?.plus(int)
+        backA = _numberA.value
+        backB = _numberB.value
+        _numberA.value = _numberA.value.plus(int)
     }
 
     fun addB(int: Int) {
-        backA = _numberA.value!!
-        backB = _numberB.value!!
-        _numberB.value = _numberB.value?.plus(int)
+        backA = _numberA.value
+        backB = _numberB.value
+        _numberB.value = _numberB.value.plus(int)
     }
 
     fun reset() {
-        backA = _numberA.value!!
-        backB = _numberB.value!!
+        backA = _numberA.value
+        backB = _numberB.value
         _numberA.value = 0
         _numberB.value = 0
     }

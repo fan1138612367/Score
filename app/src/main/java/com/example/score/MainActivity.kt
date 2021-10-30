76f53508b110fa38e03aa7fa7b550160019bd225
fan1138612367 <fan1138612367@vip.qq.com>
2021-10-30 10:35:48 +0800

    优化binding初始化

--- app/src/main/java/com/example/score/MainActivity.kt
+++ app/src/main/java/com/example/score/MainActivity.kt
@@ -7,13 +7,13 @@ import androidx.databinding.DataBindingUtil
 import com.example.score.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
     private val myViewModel by viewModels<MyViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
         binding.myViewModel = myViewModel
         binding.lifecycleOwner = this
     }

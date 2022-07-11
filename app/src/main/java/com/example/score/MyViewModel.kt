3b160b43f683bdffa6bfda7205ddab6906804d03
Fan <fan1138612367@vip.qq.com>
2022-07-11 19:25:22 +0800

    再重构StateFlow

--- app/src/main/java/com/example/score/MyViewModel.kt
+++ app/src/main/java/com/example/score/MyViewModel.kt
@@ -4,32 +4,42 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 class MyViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    private var _numberA
+        get() = numberA.value
+        set(value) {
+            savedStateHandle["numberA"] = value
+        }
     val numberA by lazy { savedStateHandle.getStateFlow("numberA", 0) }
+    private var _numberB
+        get() = numberB.value
+        set(value) {
+            savedStateHandle["numberB"] = value
+        }
     val numberB by lazy { savedStateHandle.getStateFlow("numberB", 0) }
     private var backA = 0
     private var backB = 0
 
     fun addA(int: Int) {
-        backA = numberA.value
-        backB = numberB.value
-        savedStateHandle["numberA"] = numberA.value + int
+        backA = _numberA
+        backB = _numberB
+        _numberA += int
     }
 
     fun addB(int: Int) {
-        backA = numberA.value
-        backB = numberB.value
-        savedStateHandle["numberB"] = numberB.value + int
+        backA = _numberA
+        backB = _numberB
+        _numberB += int
     }
 
     fun reset() {
         backA = numberA.value
         backB = numberB.value
-        savedStateHandle["numberA"] = 0
-        savedStateHandle["numberB"] = 0
+        _numberA = 0
+        _numberB = 0
     }
 
     fun undo() {
-        savedStateHandle["numberA"] = backA
-        savedStateHandle["numberB"] = backB
+        _numberA = backA
+        _numberB = backB
     }
 }
